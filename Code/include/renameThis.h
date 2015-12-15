0d240e9663da34030a86925d8ac80038f7ed047d
danycraft98 <giantsmart98@hotmail.com>
2015-12-15 11:09:56 -0500

    header and class
    header and class

---
+++ Code/include/renameThis.h
@@ -0,0 +1,14 @@
+#ifndef AUSTIN EXP1_H
+#define AUSTIN EXP1_H
+
+
+class austin exp1
+{
+    public:
+        austin exp1();
+        virtual ~austin exp1();
+    protected:
+    private:
+};
+
+#endif // AUSTIN EXP1_H
---
+++ Code/src/renameThis.cpp
@@ -0,0 +1,11 @@
+#include "austin exp1.h"
+
+austin exp1::austin exp1()
+{
+    //ctor
+}
+
+austin exp1::~austin exp1()
+{
+    //dtor
+}
