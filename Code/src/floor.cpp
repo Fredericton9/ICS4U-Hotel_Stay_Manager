729f27c6175af581b1d5adda6bfb6e3438721788
danycraft98 <giantsmart98@hotmail.com>
2015-12-16 12:22:31 -0500

    Update floor.cpp

--- Code/src/floor.cpp
+++ Code/src/floor.cpp
@@ -1,6 +1,11 @@
+#include <string>
 #include "floor.h"
 
 floor::floor(int floorNum, int roomTypeNum[][2]){
     int floorNum = floorNum;
     int roomTypeNum[][2] = roomTypeNum;
+}
+
+int addCustomer(string roomType){
+    int roomType[][2] = roomTypeNum;
 }
