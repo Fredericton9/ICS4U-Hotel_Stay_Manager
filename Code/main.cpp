c87fa343d47968615318de4d95ce984f3f59053a
danycraft98 <giantsmart98@hotmail.com>
2015-12-27 19:27:36 -0500

    Update main.cpp

--- Code/main.cpp
+++ Code/main.cpp
@@ -1,5 +1,14 @@
 #include "renameThis.h"
 
+int roomTypeToInt(string roomType){
+  string roomTypes[4] = {'ocean view double','ocean view single','regular double','regular single'};
+  for(int x = 0; x < 3; x++){
+    if(roomType == roomTypes[x]){
+      return x;
+    }
+  }
+}
+
 int main{
 
 }
