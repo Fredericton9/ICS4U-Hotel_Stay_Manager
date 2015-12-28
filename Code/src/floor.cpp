f50d0247f8c22f4f4fa21bf1df275f4c855d48f3
danycraft98 <giantsmart98@hotmail.com>
2015-12-27 19:31:06 -0500

    Update floor.cpp

--- Code/src/floor.cpp
+++ Code/src/floor.cpp
@@ -2,24 +2,22 @@
 #include "floor.h"
 
 floor::floor(int floorNum, int roomTypeNum[]){
-    string roomTypes[4] = {'ocean view double','ocean view single','regular double','regular single'};
     int floorNum = floorNum;
     int roomTypeNum[4] = roomTypeNum;
+    int hours[4][]
 }
 
-bool floor::roomAvailable(string roomType, int pinCode){
-    for(int x = 0; x < 3; x++){
-        if(roomType == roomTypes[x] and roomTypeNum[x] != 0){
-            return true;
-            int pinCode = pinCode;
-        }else{
-            return false;
-        }
+bool floor::roomAvailable(int roomTypeCode, int pinCode){
+    if(roomType == roomTypes[x] and roomTypeNum[roomTypeCode] != 0){
+        int pinCode = pinCode;
+        return true;
+    }else{
+        return false;
     }
 }
 
-void floor::checkIn(int days){
-    int days = days
+void floor::checkIn(){
+    
 }
 
 void floor::checkOut(){
