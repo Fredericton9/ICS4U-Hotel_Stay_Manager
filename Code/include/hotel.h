60948af2357635ca8da440c4ae2f14b20030062c
danycraft98 <giantsmart98@hotmail.com>
2016-01-06 12:09:40 -0500

    Create hotel.h

---
+++ Code/include/hotel.h
@@ -0,0 +1,12 @@
+#include <string>
+#ifndef HOTEL_H
+#define HOTEL_H
+
+class hotel
+{
+    public:
+        hotel(int floorNum, int);
+        void checkIn(string roomType);
+        void checkOut();
+
+#endif // FLOOR_H
