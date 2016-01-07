4ae2a7800462ca218c58112e0059d498beb643e8
danycraft98 <giantsmart98@hotmail.com>
2016-01-06 20:16:30 -0500

    Update hotel.cpp

--- Code/src/hotel.cpp
+++ Code/src/hotel.cpp
@@ -1,9 +1,19 @@
 #include <string>
 #include "hotel.h"
 
-hotel::hotel(){
-    int floorNum;
-    int totalRooms;
-    int splRooms;
-    int regRooms;
+hotel::hotel();
+
+void hotel::chgSetting(int floorNum, int regRooms, int splRooms){
+    if(totalRooms ){
+        int hotel[floorNum][2];
+        for(int i = 0; i < floorNum; i++) {
+            hotel[i][0] = regRooms;
+            hotel[i][1] = splRooms;
+        }
+        int totalRooms = totalRooms;
+        splRooms = splRooms;
+        regRooms = totalRooms - splRooms;
+    }else{
+        cout << "There are customers still staying at the hotel. are you sure? (y/n) ";
+    }
 }
