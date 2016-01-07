81d96bfa43714bbbfbacd872425e00f49108c5eb
danycraft98 <giantsmart98@hotmail.com>
2016-01-06 20:20:52 -0500

    Update hotel.cpp

--- Code/src/hotel.cpp
+++ Code/src/hotel.cpp
@@ -1,19 +1,28 @@
+#include <iostream>
 #include <string>
 #include "hotel.h"
+using namespace std;
 
-hotel::hotel();
+hotel::hotel(){
+    totalRooms;
+    bool IsEmpty = true;
+    string answer;
+}
 
 void hotel::chgSetting(int floorNum, int regRooms, int splRooms){
-    if(totalRooms ){
+    if(IsEmpty){
         int hotel[floorNum][2];
         for(int i = 0; i < floorNum; i++) {
             hotel[i][0] = regRooms;
             hotel[i][1] = splRooms;
         }
-        int totalRooms = totalRooms;
-        splRooms = splRooms;
-        regRooms = totalRooms - splRooms;
+        int totalRooms = (regRooms + splRooms) * floorNum;
     }else{
-        cout << "There are customers still staying at the hotel. are you sure? (y/n) ";
+        cout << "There are customers still staying at the hotel. are you sure you want to edit? (y/n) ";
+        cin >> answer;
+        if(answer == 'y'){
+            IsEmpty = true
+            chgSetting(int floorNum, int regRooms, int splRooms)
+        }
     }
 }
