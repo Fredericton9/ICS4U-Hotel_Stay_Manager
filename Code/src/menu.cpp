75dd2f71e9b6afd609695ecfbcfbfbe08c8bee1a
danycraft98 <giantsmart98@hotmail.com>
2016-01-08 10:55:51 -0500

    Update menu.cpp

--- Code/src/menu.cpp
+++ Code/src/menu.cpp
@@ -4,4 +4,13 @@
 using namespace std;
 
 menu::menu(){
+    
+}
+
+void menu::display(){
+    cout << "-------MENU-------" << endl;
+    cout << "1. Hotel Setting" << endl;
+    cout << "2. Check-in" << endl;
+    cout << "3. Edit Your Stay" << endl;
+    cout << "------------------" << endl;
 }
