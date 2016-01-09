e24acb2062e86477c410273db8988640078d9e2c
danycraft98 <giantsmart98@hotmail.com>
2016-01-08 22:29:22 -0500

    Update menu.cpp

--- Code/src/menu.cpp
+++ Code/src/menu.cpp
@@ -3,7 +3,9 @@
 #include "menu.h"
 using namespace std;
 
-menu::menu();
+menu::menu(string password){
+    string password = password;
+}
 
 int menu::display(){
     int answer = -1;
@@ -12,8 +14,21 @@ int menu::display(){
     cout << "2. Check-in" << endl;
     cout << "3. Edit Your Stay" << endl;
     cout << "------------------" << endl;
-    while(answer > 3 or answer < 0){
-        cin >> answer;
-    }
+    cin >> answer;
     return answer
+}
+
+int menu::checkInDisplay(int numFloor){
+    int info[3];
+    cout << "Which floor would you like to stay? (1 - " << numFloor << ") " << endl;
+    cin >> answer;
+    info[0] = answer;
+    cout << "Which room would you like to stay?" << endl;
+    cout << "1. Special Room" << endl;
+    cout << "2. Regular Room" << endl;
+    cin >> answer;
+    info[1] = answer;
+    cout << "How long do you plan to stay here? " << endl;
+    cin >> answer;
+    info[2] = answer;
 }
