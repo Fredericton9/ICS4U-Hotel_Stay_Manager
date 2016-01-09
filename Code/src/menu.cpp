4ad609854f387334659290809260063e995bcabf
danycraft98 <giantsmart98@hotmail.com>
2016-01-08 20:00:56 -0500

    Update menu.cpp

--- Code/src/menu.cpp
+++ Code/src/menu.cpp
@@ -3,14 +3,17 @@
 #include "menu.h"
 using namespace std;
 
-menu::menu(){
-    
-}
+menu::menu();
 
-void menu::display(){
+int menu::display(){
+    int answer = -1;
     cout << "-------MENU-------" << endl;
     cout << "1. Hotel Setting" << endl;
     cout << "2. Check-in" << endl;
     cout << "3. Edit Your Stay" << endl;
     cout << "------------------" << endl;
+    while(answer > 3 or answer < 0){
+        cin >> answer;
+    }
+    return answer
 }
