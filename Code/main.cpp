af2fd5ac46f25ffdef9f60f33ad9c6e3b18c10f4
danycraft98 <giantsmart98@hotmail.com>
2016-01-09 16:48:49 -0500

    Update main.cpp

--- Code/main.cpp
+++ Code/main.cpp
@@ -2,15 +2,6 @@
 #include "menu.h"
 #include <string>
 
-/*int roomTypeToInt(string roomType){
-    string roomTypes[4] = {'regular','special'};
-    for(int code = 0; code < 2; code++){
-        if(roomType == roomTypes[x]){
-            return code;
-        }
-    }
-}*/
-
 int main{
 
 }
