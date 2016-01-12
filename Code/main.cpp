828d6c97b99865a4753b6b93c5b064a95f96ebeb
CLGFrostless <meklordyugioh25@gmail.com>
2016-01-12 11:30:26 -0500

    V.06
    Progression.

--- Code/main.cpp
+++ Code/main.cpp
@@ -7,7 +7,7 @@
 std namespace;
 
 int main{
-    int adminPass;
+    int adminPass, numFloor, totalRooms;
     char dayConfirm;
     
     //Admin is prompted for a password before pass.
@@ -15,14 +15,26 @@ int main{
     cout << "Please create the number password for adminstrative privileges: ";
     cin >> adminPass;
     
+    //Initizalized the hotel layout variables, and plugs them into the first generated building.
+    cout << "Before we start, we would like to know; What are the current number of floors? ";
+    cin >> numFloor;
+    
+    cout << "What is the total number of rooms PER floor? Note that it MUST be an even number, else your hotel doesn't make sense. ";
+    cin >> totalRooms;
+    
     //Creates class objects
     menu menuObj;
     hotel hotelObj;
+    hotelObj.chgSetting(numFloor, totalRooms);
     
     cout << "Would you like to start the day? (y/n) ";
     cin >> dayConfirm;
     
     while(dayConfirm == "y"){
-        
+        int currentDisplay = menuObj.display();
+        if (currentDisplay == 1){
+            cout << "Please enter the administrative password in order to change your settings. ";
+            
+        }
     }
 }
