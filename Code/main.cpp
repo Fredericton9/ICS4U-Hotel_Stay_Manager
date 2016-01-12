0a3776dae57b2ebb0b3120e10d2919287329a4e4
CLGFrostless <meklordyugioh25@gmail.com>
2016-01-12 11:21:51 -0500

    V.05
    Begun progress.

--- Code/main.cpp
+++ Code/main.cpp
@@ -1,7 +1,28 @@
 #include "hotel.h"
 #include "menu.h"
+
 #include <string>
+#include <iostream>
+
+std namespace;
 
 int main{
-
+    int adminPass;
+    char dayConfirm;
+    
+    //Admin is prompted for a password before pass.
+    cout << "Good morning/afternoon/evening! Welcome to Hotel Management Assistant v1.0.0" << endl;
+    cout << "Please create the number password for adminstrative privileges: ";
+    cin >> adminPass;
+    
+    //Creates class objects
+    menu menuObj;
+    hotel hotelObj;
+    
+    cout << "Would you like to start the day? (y/n) ";
+    cin >> dayConfirm;
+    
+    while(dayConfirm == "y"){
+        
+    }
 }
