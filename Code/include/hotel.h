29f711126d6ca8db96fe466073f686ca3b5002eb
danycraft98 <giantsmart98@hotmail.com>
2016-01-14 21:37:36 -0500

    Update hotel.h

--- Code/include/hotel.h
+++ Code/include/hotel.h
@@ -4,20 +4,24 @@
 using namespace std;
 
 class hotel{
-    public:
+    private:
         int roomTypeNum;
         char answer;
         int numFloorGb;
+        int totalRoomsGb;
         int checkInNum;
         int** hotelLayout;
 
+    public:
         hotel();
         void chgSetting(int numFloor, int totalRooms);
+        void hotelLayoutDisplay();
         void timeTick();
-        void checkIn(int roomTypeCode, int floorNum, int timeStay);
+        int checkIn(int roomTypeCode, int floorNum, int timeStay);
         void changeRoom(int floorNum1,  int num1, int floorNum2, int num2);
         void changeTime(int floorNum, int num, int addTimeStay);
         void checkOut(int floorNum, int num);
+        bool isFull();
 };
 
 #endif // HOTEL_H
