46b6e192bd30e413e480bc9685bb2991f89674c6
danycraft98 <giantsmart98@hotmail.com>
2016-01-14 21:38:17 -0500

    Update menu.cpp

--- Code/src/menu.cpp
+++ Code/src/menu.cpp
@@ -13,41 +13,59 @@ int menu::display(){
     cout << "2. Check-in" << endl;
     cout << "3. Edit Your Stay" << endl;
     cout << "------------------" << endl;
-    cin >> answer;
+    while(answer > 3 or answer < 0){
+        cout << "Choice: ";
+        cin >> answer;
+    }
+    cout << endl << endl;
+    return answer;
+}
+
+int menu::adminDisplay(){
+    answer = -1;
+    cout << endl;
+    cout << "-------ADMIN MENU-------" << endl;
+    cout << "1. Change Hotel Layout" << endl;
+    cout << "2. Status of the Hotel" << endl;
+    cout << "------------------------" << endl;
+    while(answer > 2 or answer < 0){
+        cout << "Choice: ";
+        cin >> answer;
+    }
+    cout << endl << endl;
     return answer;
 }
 
 int* menu::checkIn(int numFloor){
     int info[3];
     cout << "Which floor would you like to stay? (1 - " << numFloor << ") ";
-    cin >> answer;
-    info[0] = answer;
-    cout << "Which room would you like to stay (1. Special Room, 2. Regular Room)? ";
-    cin >> answer;
-    info[1] = answer;
+    cin >> info[0];
+    cout << "Which room would you like to stay (1. Regular Room, 2. Special Room)? ";
+    cin >> info[1];
     cout << "How long do you plan to stay here? ";
-    cin >> answer;
-    info[2] = answer;
+    cin >> info[2];
     return info;
 }
 
 int* menu::enterCode(){
     int code[2];
     cout << "Floor Number: ";
-    cin >> answer;
-    code[0] = answer;
-    cout << "Room Number: " << endl;
-    cin >> answer;
-    code[1] = answer;
+    cin >> code[0];
+    cout << "Room Number: ";
+    cin >> code[1];
     return code;
 }
 
 int menu::editDisplay(){
     int answer = -1;
-    cout << "How do you want to edit your stay" << endl;
+    cout << "----Edit Stay----" << endl;
     cout << "1. Change Rooms" << endl;
     cout << "2. Change Time" << endl;
     cout << "3. Check Out" << endl;
-    cin >> answer;
+    cout << "----------------" << endl;
+    while(answer > 3 or answer < 0){
+        cout << "Choice: ";
+        cin >> answer;
+    }
     return answer;
 }
